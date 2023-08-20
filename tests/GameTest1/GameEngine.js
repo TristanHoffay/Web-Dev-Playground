8cba42ef2f0f9b122c67b2662446f55a57147763
TristanHoffay <38638607+TristanHoffay@users.noreply.github.com>
2023-08-20 13:56:53 -0700

    Basic Game and Engine Progress
    Needs level logic to be changed to be based on canvas size, and for game to restart when player falls below screen.

--- tests/GameTest1/GameEngine.js
+++ tests/GameTest1/GameEngine.js
@@ -1,6 +1,19 @@
 var physicsObjects = []; // 1D array of physics objects
+physicsObjects.name = "phyicsObjects";
 var renderedObjects = []; // 2D array of render layers (0 is background)
+renderedObjects.name = "renderedObjects";
 var actionObjects = []; // list of action scripts to run
+actionObjects.name = "actionObjects";
+var objects = [];
+objects.name = "objects";
+var textobjects = [];
+textobjects.name = "textobjects";
+var deltatime = 1000/60;
+const d = new Date();
+var lastframe = d.getTime();
+
+
+const debugLevel = 1; // 0 shows no logs, 1 shows single events, 2 shows logs every frame
 
 function startGame() {
     // Add flexible fps from input bar
@@ -14,38 +27,50 @@ function startGame() {
     }
 }
 function updateFPS(value) {
-    if (myGameArea.interval2) {
-        clearInterval(myGameArea.interval2);
-        myGameArea.interval2 = setInterval(updateRender, 1000/value);
+    if (scene.renderInterval) {
+        clearInterval(scene.renderInterval);
+        scene.renderInterval = setInterval(render, 1000/value);
     }
+    // if (scene.physicsInterval) {
+    //     clearInterval(scene.physicsInterval);
+    //     scene.physicsInterval = setInterval(update, 1000/value);
+    // }
 }
 function update() { // physics update (NOT FRAME RENDERING)
     scene.frameNo++;
     // Do any everyinterval logic here
 
     // Update physics
-    for (i = 0; i < physicsObjects.length; i++) {
-        physicsObjects[i].update();
+    for (p = 0; p < physicsObjects.length; p++) {
+        physicsObjects[p].update();
     }
     for (i = 0; i < actionObjects.length; i++) {
         actionObjects[i].updatefixedaction();
     }
 }
 function render() { // frame update
+    deltatime = d.getTime()-lastframe; // get time in ms since last frame
     scene.clear();
     for (i = 0; i < renderedObjects.length; i++) {
         for (j = 0; j < renderedObjects[i].length; j++) {
             renderedObjects[i][j].render();
         }
     }
+    for (t = 0; t < textobjects.length; t++) {
+        textobjects[t].render();
+    }
     for (i = 0; i < actionObjects.length; i++) {
         actionObjects[i].updateaction();
     }
+    // update input
+    scene.mouseDown = false;
     scene.keysdown = [];
 }
-function camera(x, y, zoom) {
+function camera(x, y, worldx, worldy, zoom) {
     this.x = x;
     this.y = y;
+    this.worldx = worldx;
+    this.worldy = worldy;
     this.zoom = zoom; // zoom should be at least 1, or objects might not appear to move smoothly
 }
 // Active scene
@@ -55,7 +80,7 @@ var scene = {
         // Create and insert canvas
         this.width = innerWidth;
         this.height = innerWidth * (9/16);
-        this.camera = new camera(this.width/2, this.height/2, 1);
+        this.camera = new camera(this.width/2, this.height/2, 0, 0, 1);
         this.canvas.width = this.width;
         this.canvas.height = this.height;
         this.context = this.canvas.getContext("2d");
@@ -68,6 +93,7 @@ var scene = {
 
         // Add listeners for mouse and keyboard
         window.addEventListener('mousedown', function (e) {
+            scene.mouseHeld = true;
             scene.mouseDown = true;
             scene.mousex = e.pageX;
             scene.mousey = e.pageY;
@@ -79,7 +105,7 @@ var scene = {
             }
         })
         window.addEventListener('mouseup', function (e) {
-            scene.mouseDown = false;
+            scene.mouseHeld = false;
         })
         window.addEventListener('keydown', function (e) {
             scene.keys = (scene.keys || []);
@@ -115,52 +141,79 @@ function object(x, y, width, height) {
     this.y = y;
     this.width = width;
     this.height = height;
-}
-// Components that can be added to an object
-function addtorenderlayer(renderer, layer) {
-    while (renderedObjects.length <= layer) {
-        renderedObjects.push([]);
+    addtolist(this, objects);
+    this.remove = function() {
+        removefromlist(this, objects);
     }
-    renderedObjects[layer].push(renderer);
+    if (debugLevel > 0) console.log("Object number " + this.index + " added.");
 }
-function physics(speedx, speedy, gravity, bounce, friction, object) {
-    this.index = physicsObjects.push(this);
+function physics(speedx, speedy, gravity, bounce, friction, object, dynamic) {
     this.object = object;
     this.gravity = gravity;
     this.friction = friction;
+    this.dynamic = dynamic;
     this.bounce = bounce; // bounce factor when collides, applied to speed in - direction
     this.speedx = speedx;
     this.speedy = speedy;
+    addtolist(this, physicsObjects);
+    if (debugLevel > 0) {
+        console.log("Physics obj added");
+        console.log("New physics aray looks like:");
+        for (i = 0; i < physicsObjects.length; i++) {
+            console.log("\n" + physicsObjects[i].object.index);
+        }
+    }
+    this.remove = function() {
+        removefromlist(this, physicsObjects);
+    }
+    this.oncollision = function(check) {
+
+    }
     this.update = function() {
-        // Update speed
-        this.speedy += this.gravity;
-        this.speedx *= friction;
-        console.log("gravity increase, speed now " + this.speedy);
-        // Check collision
-        var check = this.checkcollisions(this.speedx, this.speedy, true);
-        console.log("check: " + check);
-        // Update position
-        if (check % 2 == 1) this.speedx *= -this.bounce;
-        this.object.x += this.speedx;
-        if (check > 1) this.speedy *= -this.bounce;
-        this.object.y += this.speedy;
-        console.log("after check speed is: " + this.speedy);
+        if (this.dynamic) {
+            if (debugLevel > 1) console.log("Updating " + this.object.index);
+            // Update speed
+            this.speedy += this.gravity;
+            this.speedx *= friction;
+            // Check collision
+            var check = this.checkcollisions(this.speedx, this.speedy, true);
+            // Update position
+            if (check % 2 == 1) this.speedx *= -this.bounce;
+            this.object.x += this.speedx;
+            if (check > 1) this.speedy *= -this.bounce;
+            this.object.y += this.speedy;
+
+            if (check) this.oncollision(check);
+        }
     }
     this.checkcollisions = function(offsetx, offsety, bounceOther) {
+        var finalcheck = 0;
         for (i = 0; i < physicsObjects.length; i++) {
-            if (i != this.index - 1) {
+            if (i != this.index) {
+                if (debugLevel > 1) console.log("Checking collisions between object " + this.object.index + " and " + physicsObjects[i].object.index);
                 var check = this.collides(offsetx, offsety, physicsObjects[i].object)
-                if (bounceOther) {
-                    if (check % 2 == 1) {
-                        physicsObjects[i].speedx *= -physicsObjects[i].bounce;
+                if (check) {
+                    if (bounceOther) {
+                        console.log("Running oncollision for object " + physicsObjects[i].object.index)
+                        physicsObjects[i].oncollision(check);
+                        if (check % 2 == 1) {
+                            physicsObjects[i].speedx *= -physicsObjects[i].bounce;
+                            if (debugLevel > 1) console.log("Object " + this.object.index + " collides with " + physicsObjects[i].object.index + " horizontally");
+                        }
+                        if (check > 1) {
+                            physicsObjects[i].speedy *= -physicsObjects[i].bounce;
+                            if (debugLevel > 1) console.log("Object " + this.object.index + " collides with " + physicsObjects[i].object.index + " vertically");
+                        }
                     }
-                    if (check > 1) {
-                        physicsObjects[i].speedy *= -physicsObjects[i].bounce;
+                    if (check == 1) finalcheck += 1;
+                    if (check == 2) finalcheck += 2;
+                    if (finalcheck > 3 || check == 3) {
+                        return 3;
                     }
                 }
-                return check;
             }
         }
+        return finalcheck;
     }
     this.collides = function(offsetx, offsety, otherobj) {
         var myleft = this.object.x + offsetx;
@@ -196,22 +249,90 @@ function physics(speedx, speedy, gravity, bounce, friction, object) {
         }
     }
 }
+function removefromlist(object, list) {
+    if (debugLevel > 0) console.log("Removing object at index " + object.index + " in " + list.name);
+    list.splice(object.index, 1); // splice out object from list
+    for (r = object.index; r < list.length; r++) { // update index value of other objects (so they know)
+        if (debugLevel > 0) console.log("Updating object at index " + list[r].index + " to index " + (list[r].index - 1));
+        list[r].index--;
+    }
+}
+function addtolist(object, list) {
+    if (debugLevel > 0) console.log("Adding object to " + list.name + " at index " + list.length);
+    object.index = list.push(object) - 1;
+}
+// Components that can be added to an object
+function addtorenderlayer(renderer, layer) {
+    if (debugLevel > 0) console.log("Adding object to render layer " + layer);
+    while (renderedObjects.length <= layer) {
+        if (debugLevel > 0) console.log("Additional layer needed, adding layer " + renderedObjects.length);
+        let newlayer = [];
+        newlayer.name = "render layer " + renderedObjects.length;
+        renderedObjects.push(newlayer);
+    }
+    addtolist(renderer, renderedObjects[layer]);
+    if (debugLevel > 0) {
+        console.log("Renderer added to layer " + (renderedObjects.length-1));
+        console.log("New 2d renderer aray looks like:");
+        for (i = 0; i < renderedObjects.length; i++) {
+            console.log("\nLayer " + i + ":\n");
+            for (j = 0; j < renderedObjects[i].length; j++) {
+                console.log(renderedObjects[i][j].object.index + ", ");
+            }
+        }
+    }
+}
 function renderer(layer, object, color) {
-    addtorenderlayer(this, layer);
+    this.layer = layer;
     this.object = object;
     this.color = color;
+    addtorenderlayer(this, layer);
+    this.render = function() {
+        if (debugLevel > 1) console.log("Rendering layer " + this.layer + " object " + this.object.index);
+        ctx = scene.context;
+        ctx.fillStyle = this.color;
+        if (this.object.physics) {
+            ctx.fillRect(
+                scene.camera.x + (this.object.x + (this.object.physics.speedx * deltatime) - scene.camera.worldx) * scene.camera.zoom, 
+                scene.camera.y + (this.object.y + (this.object.physics.speedy * deltatime) - scene.camera.worldy) * scene.camera.zoom, 
+                this.object.width * scene.camera.zoom, this.object.height * scene.camera.zoom);
+        }
+        else {
+            ctx.fillRect(
+                scene.camera.x + (this.object.x - scene.camera.worldx) / scene.camera.zoom, 
+                scene.camera.y + (this.object.y - scene.camera.worldy) / scene.camera.zoom, 
+                this.object.width * scene.camera.zoom, this.object.height * scene.camera.zoom);
+        }
+    } // should be altered to cull things outside frame
+    this.remove = function() {
+        //remove from array renderedObjects[layer]
+        removefromlist(this, renderedObjects[this.layer]);
+    }
+}
+function textbox(size, font, color, x, y, text) {
+    this.size = size;
+    this.font = font;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.text = text;
+    addtolist(this, textobjects);
     this.render = function() {
         ctx = scene.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(
-            scene.camera.x + this.object.x / scene.camera.zoom, 
-            scene.camera.y + this.object.y / scene.camera.zoom, 
-            this.object.width, this.object.height);
-    } // should be altered to cull things outside frame
+        ctx.font = this.size + " " + this.font;
+        ctx.fillStyle = this.color;
+        ctx.fillText(this.text, this.x, this.y);
+    }
+    this.remove = function() {
+        removefromlist(this, textobjects);
+    }
 }
 function action(object) {
     this.object = object;
-    actionObjects.push(this);
+    addtolist(this, actionObjects);
+    this.remove = function() {
+        removefromlist(this, actionObjects);
+    }
     this.startaction = function() {
 
     }
--- tests/GameTest1/game1.js
+++ tests/GameTest1/game1.js
@@ -1,6 +1,9 @@
 // Create and modify objects and stuff here for specific game usage
 var player;
-var ground;
+var platforms = [];
+platforms.name = "platforms";
+var lastplatformx = 500;
+var score;
 
 
 function TestGame() {
@@ -17,25 +20,88 @@ function TestGame() {
     // }
     // player.action = playerAction;
 
+    // Create score
+    score = new textbox("30px", "Verdana", "black", 100, 40, "Score: 0");
+    score.score = 0;
     // Create a player
     player = new object(0, 0, 50, 50);
     player.renderer = new renderer(2, player, "red");
-    player.physics = new physics(0,0,0.5,0.2, 0.8, player);
+    player.physics = new physics(0,0,0.8,0, 0.8, player, true);
     var playerAction = new action(player);
-    playerAction.jumping = false;
+    playerAction.jumping = 0;
+    playerAction.nextHeight = 100;
+    // player.physics.oncollision = function(check) {
+    //     if (check > 1)
+    //         playerAction.jumping = false;
+    // }
     playerAction.updateaction = function() {
+        if (scene.keys && scene.keys["I"]) {scene.camera.zoom *= 1.01}
+        if (scene.keys && scene.keys["O"]) {scene.camera.zoom /= 1.01}
         if (scene.keys && scene.keys["A"]) {player.physics.speedx = -10;}
         if (scene.keys && scene.keys["D"]) {player.physics.speedx = 10;}
         if (scene.keysdown && scene.keysdown["W"] && player.physics.checkcollisions(0, 10, false)) 
-        {player.physics.speedy = -15;}
+        {player.physics.speedy = -10; playerAction.jumping = scene.frameNo;}
+        if (scene.keys && scene.keys["W"] && playerAction.jumping > scene.frameNo - 20) { player.physics.speedy = -10;}
+        if (player.y < scene.camera.worldy) {
+            scene.camera.worldy = player.y
+        }
+        if (player.y < score.score) {
+             score.score = player.y;
+             score.text = "Score: " + -1 * Math.floor(score.score / 100);
+        }
+        if (player.y < playerAction.nextHeight + 200) {
+            playerAction.nextHeight -= 200;
+            let nextx = (Math.random() * 1000) - 500;
+            nextx = nextx < lastplatformx-400 ? lastplatformx-400 : nextx > lastplatformx + 400 ? lastplatformx + 400 : nextx;
+            lastplatformx = nextx;
+            platform(nextx, playerAction.nextHeight, 100, 20);
+            platform(Math.random() * 1000 - 500, playerAction.nextHeight, 100, 20);
+            
+            fallingplatform((Math.random() * 1000) - 500, playerAction.nextHeight - 100, 100, 20);
+        }
     }
     player.action = playerAction;
 
-    // Create ground
-    ground = new object(-500, 100, 1000, 100);
-    ground.renderer = new renderer(1, ground, "green");
-    ground.physics = new physics(0, 0, 0, 0, 0, ground);
 
+    // Create ground
+    function platform (x, y, w, h) {
+        let ground = new object(x, y, w, h);
+        ground.renderer = new renderer(1, ground, "green");
+        ground.physics = new physics(0, 0, 0, 0, 0, ground, false);
+        addtolist(this, platforms);
+        var platAction = new action(ground);
+        platAction.updateaction = function() {
+            if (scene.camera.worldy + 200 < ground.y) {
+                ground.renderer.remove();
+                ground.physics.remove();
+                ground.remove();
+                removefromlist(this, platforms);
+                platAction.remove();
+            }
+        }
+    }
+    function fall(physicobject) {
+        physicobject.gravity = 1;
+        physicobject.dynamic = true;
+    }
+    function fallingplatform (x, y, w, h) {
+        let ground = new object(x, y, w, h);
+        ground.renderer = new renderer(1, ground, "yellow");
+        ground.physics = new physics(0, 0, 0, 0, 0, ground, false);
+        ground.physics.oncollision = function(check) {
+            ground.physics.object.renderer.color = "red";
+            setTimeout(fall, 500, ground.physics);
+        }
+        addtolist(this, platforms);
+        // var platAction = new action(ground);
+        // platAction.updateaction = function() {
+        //     if (scene.camera.worldy + 50 < ground.y) {
+        //         ground.renderer.remove();
+        //     }
+        // }
+    }
+    platform(-500, 100, 1000, 100);
+    
 
     startGame();
 }
